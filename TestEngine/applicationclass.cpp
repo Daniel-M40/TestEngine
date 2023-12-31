c92bbcc89e615f5d4331f36a98a32f77bbf5c0c9
Daniel-M40 <daniel.moir0993@gmail.com>
2023-12-31 10:12:38 +0000

    created a PS, VS, Camera and model class

--- TestEngine/applicationclass.cpp
+++ TestEngine/applicationclass.cpp
@@ -7,6 +7,9 @@
 ApplicationClass::ApplicationClass()
 {
 	m_Direct3D = nullptr;
+	m_Camera = nullptr;
+	m_Model = nullptr;
+	m_ColorShader = nullptr;
 }
 
 
@@ -34,6 +37,32 @@ bool ApplicationClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 		MessageBox(hwnd, L"Could not initialize Direct3D", L"Error", MB_OK);
 	}
 
+	// Create the camera object.
+	m_Camera = new CameraClass;
+
+	// Set the initial position of the camera.
+	m_Camera->SetPosition(0.0f, 0.0f, -15.0f);
+
+	// Create and initialize the model object.
+	m_Model = new ModelClass;
+
+	result = m_Model->Initialize(m_Direct3D->GetDevice());
+	if (!result)
+	{
+		MessageBox(hwnd, L"Could not initialize the model object.", L"Error", MB_OK);
+		return result;
+	}
+
+	// Create and initialize the color shader object.
+	m_ColorShader = new ColorShaderClass;
+
+	result = m_ColorShader->Initialize(m_Direct3D->GetDevice(), hwnd);
+	if (!result)
+	{
+		MessageBox(hwnd, L"Could not initialize the color shader object.", L"Error", MB_OK);
+		return result;
+	}
+
 	return result;
 }
 
@@ -47,6 +76,29 @@ void ApplicationClass::Shutdown()
 		m_Direct3D->Shutdown();
 		delete m_Direct3D;
 		m_Direct3D = nullptr;
+	}
+
+	// Release the color shader object.
+	if (m_ColorShader)
+	{
+		m_ColorShader->Shutdown();
+		delete m_ColorShader;
+		m_ColorShader = nullptr;
+	}
+
+	// Release the model object.
+	if (m_Model)
+	{
+		m_Model->Shutdown();
+		delete m_Model;
+		m_Model = nullptr;
+	}
+
+	// Release the camera object.
+	if (m_Camera)
+	{
+		delete m_Camera;
+		m_Camera = nullptr;
 	}
 
 	return;
@@ -64,9 +116,30 @@ bool ApplicationClass::Frame()
 
 bool ApplicationClass::Render()
 {
-	// Clear the buffers to begin the scene.
-	m_Direct3D->BeginScene(0.1f, 0.1f, 0.5f, 1.0f);
+	XMMATRIX worldMatrix, viewMatrix, projectionMatrix;
+	bool result;
 
+
+	// Clear the buffers to begin the scene.
+	m_Direct3D->BeginScene(0.0f, 0.0f, 0.0f, 1.0f);
+
+	// Generate the view matrix based on the camera's position.
+	m_Camera->Render();
+
+	// Get the world, view, and projection matrices from the camera and d3d objects.
+	m_Direct3D->GetWorldMatrix(worldMatrix);
+	m_Camera->GetViewMatrix(viewMatrix);
+	m_Direct3D->GetProjectionMatrix(projectionMatrix);
+
+	// Put the model vertex and index buffers on the graphics pipeline to prepare them for drawing.
+	m_Model->Render(m_Direct3D->GetDeviceContext());
+
+	// Render the model using the color shader.
+	result = m_ColorShader->Render(m_Direct3D->GetDeviceContext(), m_Model->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix);
+	if (!result)
+	{
+		return false;
+	}
 
 	// Present the rendered scene to the screen.
 	m_Direct3D->EndScene();
